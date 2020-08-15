ce7e180c04e45e8a9b06d0a36804cca9d54ea1b9
Ane <61033884+anedejour@users.noreply.github.com>
2020-08-15 11:33:31 -0300

    Add files via upload

---
+++ coding-bootcamp-testimonials-slider-master/index.js
@@ -0,0 +1,5 @@
+function Funcprev(){
+    if(document.getElementById('prev').click){
+        document.getElementById('tanya').stayle.display = "none";
+        document.getElementById('john').stayle.display = "inline";
+    }
